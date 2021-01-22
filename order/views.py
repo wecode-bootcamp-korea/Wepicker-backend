d451decc610ab6b4b9d84daf19621b211aacce95
redsaumon <ywhong830@gmail.com>
2021-01-22 10:44:32 +0900

    Fix: 장바구니 담기 수습2

--- order/views.py
+++ order/views.py
@@ -18,7 +18,7 @@ class OrderItemView(View):
             data          = json.loads(request.body)
             option_list   = data.get('option_list')
             delivery_cost = data['delivery_cost']
-
+            
             if delivery_cost[0] == 2:
                   delivery_cost = 3
             else:
@@ -27,8 +27,7 @@ class OrderItemView(View):
                 else:
                     delivery_cost = 2
 
-            if Order.objects.filter(user=request.user.id, state_id=1, orderItem__product_id=data['product'], \
-                orderItem__delivery_cost_id=delivery_cost).exists():
+            if Order.objects.filter(user=request.user.id, state_id=1, orderItem__delivery_cost_id=delivery_cost).exists():
 
                 order_item, created = OrderItem.objects.get_or_create(
                         order__user_id=request.user.id, 
@@ -36,8 +35,10 @@ class OrderItemView(View):
                         product_id=data['product'], 
                         delivery_cost_id=delivery_cost
                 )
-                
+               
                 if created:   # 기존 ORDER 존재 / 다른 상품 추가
+                    print(order_item.order_id)
+                    order_item.order_id   = order_item.order_id
                     order_item.product_id = data["product"]
                     order_item.option     = option_list
                     order_item.quantity   = data["qantity"]
@@ -48,19 +49,21 @@ class OrderItemView(View):
 
                 for input_option in option_list: #기존 ORDER 존재 / 상품 업데이트(수량, 갸격)
                     for option in order_item.option: # 옵션까지 일치할 경우 수량, 가격 더해주기
-                        if input_option["option_id"] == option["option_id"]:
+                        if input_option['option_id'] == option["option_id"]:
                             order_item.quantity                     = order_item.quantity + data['quantity']
-                            order_item.price                        = order_item.price + data['price']
+                            order_item.price                        = order_item.price + int(data['price'])
                             order_item.option[0]['option_quantity'] = order_item.option[0]['option_quantity'] + input_option['option_quantity']
                             order_item.save()
                         else: # 새로운 옵션 추가
                             order_item.quantity = order_item.quantity + data['quantity']
-                            order_item.option   = order_item.option + input_option
+                            order_item.price    = order_item.price + int(data['price'])
+                            order_item.option.append(input_option)
                             order_item.save()
+                            return JsonResponse({'message':'SUCCESS'}, status=201)
                 return JsonResponse({'message':'SUCCESS'}, status=201)
 
             # 처음 장바구니에 담을 경우
-            if Address.objects.filter(user=request.user).filter(default=True).exists():
+            if Address.objects.filter(user=request.user, default=True).exists():
                 address = Address.objects.filter(user=request.user).get(default=True)
             else:
                 address = data.get('address')
@@ -102,7 +105,7 @@ class OrderItemView(View):
                 'product'       : order_item.product.id,
                 'quantity'      : order_item.quantity,
                 'price'         : order_item.price,
-                'option'        : order_item.option.id,
+                'option'        : order_item.option,
                 'delivery_cost' : order_item.delivery_cost.id
             }for order_item in order.orderItem.all()]
 
